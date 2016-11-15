e684168548fbe5763b21f791c9c5b623d3009cfe
sendername <sendername@yandex.ru>
2016-11-15 19:28:18 +0300

    add method - initStructs

--- Vulkan_edu/Vulkan_edu/WP_Vulkan.cpp
+++ Vulkan_edu/Vulkan_edu/WP_Vulkan.cpp
@@ -4,9 +4,30 @@
 
 WP_Vulkan::WP_Vulkan()
 {
+	this->pAppInfo = new VkApplicationInfo();
+	this->pInstCrtInfo = new VkInstanceCreateInfo();
 }
-
 
 WP_Vulkan::~WP_Vulkan()
 {
+}
+
+void WP_Vulkan::initStructs()
+{
+	pAppInfo->apiVersion = VK_MAKE_VERSION(1, 0, 0);
+	pAppInfo->applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	pAppInfo->engineVersion = VK_API_VERSION_1_0;
+	pAppInfo->pApplicationName = "My learning the Vulkan";
+	pAppInfo->pEngineName = "Tutorial engine";
+	pAppInfo->pNext = nullptr;
+	pAppInfo->sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	
+	pInstCrtInfo->enabledExtensionCount = 0;
+	pInstCrtInfo->enabledLayerCount = 0;
+	pInstCrtInfo->flags = 0;
+	pInstCrtInfo->pApplicationInfo = this->pAppInfo;
+	pInstCrtInfo->pNext = nullptr;
+	pInstCrtInfo->ppEnabledExtensionNames = nullptr;
+	pInstCrtInfo->ppEnabledLayerNames = nullptr;
+	pInstCrtInfo->sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 }
--- Vulkan_edu/Vulkan_edu/WP_Vulkan.h
+++ Vulkan_edu/Vulkan_edu/WP_Vulkan.h
@@ -1,8 +1,12 @@
+#include<vulkan\vulkan.h>
 #pragma once
 class WP_Vulkan
 {
+	VkApplicationInfo *pAppInfo;
+	VkInstanceCreateInfo *pInstCrtInfo;
 public:
 	WP_Vulkan();
 	~WP_Vulkan();
+	void initStructs()
 };
 
