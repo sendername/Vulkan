a926ac9418e92de173521896803ad04aa214908b
sendername <sendername@yandex.ru>
2016-11-15 19:37:05 +0300

    add create instance

--- Vulkan_edu/Vulkan_edu/WP_Vulkan.cpp
+++ Vulkan_edu/Vulkan_edu/WP_Vulkan.cpp
@@ -1,10 +1,9 @@
 #include "WP_Vulkan.h"
 
 
-
 WP_Vulkan::WP_Vulkan()
 {
-	this->pAppInfo = new VkApplicationInfo();
+	this->pCreateInfo = new VkApplicationInfo();
 	this->pInstCrtInfo = new VkInstanceCreateInfo();
 }
 
@@ -14,20 +13,25 @@ WP_Vulkan::~WP_Vulkan()
 
 void WP_Vulkan::initStructs()
 {
-	pAppInfo->apiVersion = VK_MAKE_VERSION(1, 0, 0);
-	pAppInfo->applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-	pAppInfo->engineVersion = VK_API_VERSION_1_0;
-	pAppInfo->pApplicationName = "My learning the Vulkan";
-	pAppInfo->pEngineName = "Tutorial engine";
-	pAppInfo->pNext = nullptr;
-	pAppInfo->sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	pCreateInfo->apiVersion = VK_MAKE_VERSION(1, 0, 0);
+	pCreateInfo->applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	pCreateInfo->engineVersion = VK_API_VERSION_1_0;
+	pCreateInfo->pApplicationName = "My learning the Vulkan";
+	pCreateInfo->pEngineName = "Tutorial engine";
+	pCreateInfo->pNext = nullptr;
+	pCreateInfo->sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 	
 	pInstCrtInfo->enabledExtensionCount = 0;
 	pInstCrtInfo->enabledLayerCount = 0;
 	pInstCrtInfo->flags = 0;
-	pInstCrtInfo->pApplicationInfo = this->pAppInfo;
+	pInstCrtInfo->pApplicationInfo = this->pCreateInfo;
 	pInstCrtInfo->pNext = nullptr;
 	pInstCrtInfo->ppEnabledExtensionNames = nullptr;
 	pInstCrtInfo->ppEnabledLayerNames = nullptr;
 	pInstCrtInfo->sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+
+	if (vkCreateInstance(pInstCrtInfo, nullptr, pInstance) == VK_SUCCESS)
+		std::cout << "Instance is created" << std::endl;
+	else
+		std::cout << "Mistakes in create -> Instance" << std::endl;
 }
--- Vulkan_edu/Vulkan_edu/WP_Vulkan.h
+++ Vulkan_edu/Vulkan_edu/WP_Vulkan.h
@@ -1,12 +1,15 @@
 #include<vulkan\vulkan.h>
+#include<iostream>
 #pragma once
 class WP_Vulkan
 {
-	VkApplicationInfo *pAppInfo;
+	VkApplicationInfo *pCreateInfo;
 	VkInstanceCreateInfo *pInstCrtInfo;
+	VkInstance *pInstance;
+	VkPhysicalDevice *pPhysicalDevice;
 public:
 	WP_Vulkan();
 	~WP_Vulkan();
-	void initStructs()
+	void initStructs();
 };
 
